e979ea8ee4b35600fb09216b9ef37bcfbcdb3856
rahulsharma-fullstack <itsmerahul.contact@gmail.com>
2025-04-23 00:17:38 -0400

    cloudinary integration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
 
 // Configure Cloudinary
 cloudinary.config({
